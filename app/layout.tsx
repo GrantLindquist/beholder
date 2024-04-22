fb3f0fe8c016b94b714180b9632f789cee6d5b2a
grant-lindquist <grant-lindquist@outlook.com>
2024-04-22 14:24:27 -0700

    Project setup & themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
